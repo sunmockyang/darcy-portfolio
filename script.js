3524957462f5c0c758beefdde80688cfe1be0fd7
Sunmock Yang <sunmock@live.ca>
2017-07-24 20:26:22 -0400

    Encapsulate javascript

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-var animateInContainers = [];
-function main() {
-	animateInContainers = document.getElementsByClassName("text-animate-container");
+var app = (function() {
+	var animateInContainers = [];
+	function main() {
+		animateInContainers = document.getElementsByClassName("text-animate-container");
 
-	processAnimateContainers();
-}
-
-// In Animation functions
-function animateInCallback() {
-	this.classList.add("text-animate-end");
-};
-
-function processAnimateContainers() {
-	var delay = 200;
-	var delayInterval = 150;
-	for (var i = 0; i < animateInContainers.length; i++) {
-		var textToAnimate = animateInContainers[i].children[0];
-		
-		if (textToAnimate) {
-			var placeholder = textToAnimate.cloneNode(true);
-			placeholder.classList.remove("text-animate");
-			placeholder.classList.add("text-animate-placeholder");
-			animateInContainers[i].appendChild(placeholder);
-
-			if (!textToAnimate.classList.contains("fade-in")) {
-				textToAnimate.style.marginTop = textToAnimate.getBoundingClientRect().height * 1.5 + "px";
-			}
-			window.setTimeout(animateInCallback.bind(textToAnimate), delay);
-			delay += delayInterval;
-		}
+		processAnimateContainers();
 	}
-};
 
-window.onload = main;
+	// In Animation functions
+	function animateInCallback() {
+		this.classList.add("text-animate-end");
+	};
+
+	function processAnimateContainers() {
+		var delay = 200;
+		var delayInterval = 150;
+		for (var i = 0; i < animateInContainers.length; i++) {
+			var textToAnimate = animateInContainers[i].children[0];
+			
+			if (textToAnimate) {
+				var placeholder = textToAnimate.cloneNode(true);
+				placeholder.classList.remove("text-animate");
+				placeholder.classList.add("text-animate-placeholder");
+				animateInContainers[i].appendChild(placeholder);
+
+				if (!textToAnimate.classList.contains("fade-in")) {
+					textToAnimate.style.marginTop = textToAnimate.getBoundingClientRect().height * 1.5 + "px";
+				}
+				window.setTimeout(animateInCallback.bind(textToAnimate), delay);
+				delay += delayInterval;
+			}
+		}
+	};
+
+	return main;
+}());
+
+window.onload = app;
