9b57989c35a89d8d7688c50700bd8737eddb6db1
Sunmock Yang <sunmock@live.ca>
2017-07-24 20:24:34 -0400

    Separate out javascript into external file

---
+++ script.js
@@ -0,0 +1,34 @@
+var animateInContainers = [];
+function main() {
+	animateInContainers = document.getElementsByClassName("text-animate-container");
+
+	processAnimateContainers();
+}
+
+// In Animation functions
+function animateInCallback() {
+	this.classList.add("text-animate-end");
+};
+
+function processAnimateContainers() {
+	var delay = 200;
+	var delayInterval = 150;
+	for (var i = 0; i < animateInContainers.length; i++) {
+		var textToAnimate = animateInContainers[i].children[0];
+		
+		if (textToAnimate) {
+			var placeholder = textToAnimate.cloneNode(true);
+			placeholder.classList.remove("text-animate");
+			placeholder.classList.add("text-animate-placeholder");
+			animateInContainers[i].appendChild(placeholder);
+
+			if (!textToAnimate.classList.contains("fade-in")) {
+				textToAnimate.style.marginTop = textToAnimate.getBoundingClientRect().height * 1.5 + "px";
+			}
+			window.setTimeout(animateInCallback.bind(textToAnimate), delay);
+			delay += delayInterval;
+		}
+	}
+};
+
+window.onload = main;
